53b64cea76069ebb08cc55912cb1a51a90283f35
fegloff <fegloff@gmail.com>
2024-02-11 21:55:42 -0500

    add basic showing post logic

--- socialMap/SocialMap/src/screens/MapViewComponent.jsx
+++ socialMap/SocialMap/src/screens/MapViewComponent.jsx
@@ -1,10 +1,20 @@
 // MapViewComponent.jsx
-import React, { useEffect, useRef, useState } from 'react';
-import { View, Text, Button, StyleSheet, Dimensions, Platform, TouchableOpacity, Alert, CheckmarkBox } from 'react-native';
-import MapView, { Marker, Callout } from 'react-native-maps';
-import { AudioRecorder, AudioUtils } from 'react-native-audio';
-import { getMapMarkers, hasTranscription } from '../apis/markers';
-import { speechToText } from '../apis/openai';
+import React, {useEffect, useRef, useState} from 'react';
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  Dimensions,
+  Platform,
+  TouchableOpacity,
+  Alert,
+  CheckmarkBox,
+} from 'react-native';
+import MapView, {Marker, Callout} from 'react-native-maps';
+import {AudioRecorder, AudioUtils} from 'react-native-audio';
+import {getMapMarkers, hasTranscription} from '../apis/markers';
+import {speechToText} from '../apis/openai';
 import Toast from 'react-native-toast-message';
 import Geolocation from '@react-native-community/geolocation';
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -14,31 +24,26 @@ const windowHeight = Dimensions.get('window').height;
 
 const MapViewComponent = () => {
   // State for managing recording
-  const [markers, setMarkers] = useState([])
+  const [markers, setMarkers] = useState([]);
   const [isRecording, setIsRecording] = useState(false);
   const [hasPermission, setHasPermission] = useState(false);
   const [checkedIn, setCheckedIn] = useState({});
-  // const [items, setItems] = useState([]);
-  // const [recordings, setRecordings] = useState([]);
-  const [audioPath, setAudioPath] = useState(AudioUtils.DocumentDirectoryPath + `/voiceMemo_${Date.now()}.mp4`);
+  const [showPosts, setShowPosts] = useState(false)
+  const [audioPath, setAudioPath] = useState(
+    AudioUtils.DocumentDirectoryPath + `/voiceMemo_${Date.now()}.mp4`,
+  );
   const mapRef = useRef(null);
-
-  // Function to add data to the array
-  // const addItem = (id, text) => {
-  //   const newItem = { id, text };
-  //   setItems(currentItems => [...currentItems, newItem]);
-  // };
 
   useEffect(() => {
     const getMarkers = async () => {
-      const markers = await getMapMarkers('ffdgdf')
-      setMarkers(markers)
-    }
-    getMarkers()
-  }, [])
+      const markers = await getMapMarkers('ffdgdf');
+      setMarkers(markers);
+    };
+    getMarkers();
+  }, []);
 
   useEffect(() => {
-    AudioRecorder.requestAuthorization().then((isAuthorised) => {
+    AudioRecorder.requestAuthorization().then(isAuthorised => {
       setHasPermission(isAuthorised);
       if (isAuthorised) {
         AudioRecorder.prepareRecordingAtPath(audioPath, {
@@ -49,27 +54,31 @@ const MapViewComponent = () => {
         });
       }
     });
-  }, [])
+  }, []);
 
   useEffect(() => {
     if (mapRef.current) {
-      mapRef.current.fitToSuppliedMarkers(markers.map(marker => marker.id), {
-        edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
-        animated: true,
-      });
+      mapRef.current.fitToSuppliedMarkers(
+        markers.map(marker => marker.id),
+        {
+          edgePadding: {top: 50, right: 50, bottom: 50, left: 50},
+          animated: true,
+        },
+      );
     }
   }, [markers]);
 
   // Handle recording start
-  const startRecording = async (marker) => {
+  const startRecording = async marker => {
     if (!hasPermission) {
       console.warn("Can't record, no permission granted!");
       return;
     }
     setIsRecording(true);
     if (hasTranscription(markers)) {
-      const audio = AudioUtils.DocumentDirectoryPath + `/voiceMemo_${Date.now()}.mp4`
-      setAudioPath(audio)
+      const audio =
+        AudioUtils.DocumentDirectoryPath + `/voiceMemo_${Date.now()}.mp4`;
+      setAudioPath(audio);
       AudioRecorder.prepareRecordingAtPath(audio, {
         SampleRate: 22050,
         Channels: 1,
@@ -81,13 +90,13 @@ const MapViewComponent = () => {
   };
 
   // Handle recording stop and playback
-  const stopRecordingAndPlayBack = async (id) => {
+  const stopRecordingAndPlayBack = async id => {
     if (!isRecording) return;
     await AudioRecorder.stopRecording();
     setIsRecording(false);
     // Playback the recording
     try {
-      console.log(audioPath)
+      console.log(audioPath);
       const result = await speechToText(audioPath);
       if (result) {
         console.log('Transcription result:', result);
@@ -96,17 +105,16 @@ const MapViewComponent = () => {
           text1: result,
         });
         const updatedMarkers = markers.map(marker =>
-          marker.id === id ? { ...marker, memoTranscription: result } : marker
+          marker.id === id
+            ? {
+                ...marker,
+                memoTranscription: marker.memoTranscription
+                  ? `${marker.memoTranscription}\n${result}`
+                  : result,
+              }
+            : marker,
         );
         setMarkers(updatedMarkers);
-        // setMarkers(...markers.map(m => {
-        //   if (m.id === id) {
-        //     m.transcription = m.transcription ? m.transcription.concat(`\n${result}`) : result;
-        //   }
-        //   return m
-        // }))
-        // addItem(id, result);
-        // setAudioPath(AudioUtils.DocumentDirectoryPath + `/voiceMemo_${Date.now()}.mp4`)
         console.log('Current data store in array', markers);
       } else {
         Toast.show({
@@ -119,27 +127,27 @@ const MapViewComponent = () => {
     }
   };
 
-  const handlePress = (marker) => {
+  const handlePress = marker => {
     console.log('Button pressed for:', marker.name);
   };
 
-  const toggleCheckIn = (id) => {
+  const toggleCheckIn = id => {
     setCheckedIn(prev => ({
       ...prev,
       [id]: !prev[id],
     }));
   };
-  
-  const handleCheckIn = (marker) => {
-    console.log('marker checked in', marker)
-  }
+
+  const handleCheckIn = marker => {
+    console.log('marker checked in', marker);
+  };
 
   const getCurrentLocation = () => {
     console.log('Attempting to get current position...');
     Geolocation.getCurrentPosition(
-      (position) => {
+      position => {
         console.log('Current position:', position);
-        const { latitude, longitude } = position.coords;
+        const {latitude, longitude} = position.coords;
         const newRegion = {
           latitude,
           longitude,
@@ -148,16 +156,15 @@ const MapViewComponent = () => {
         };
         mapRef.current.animateToRegion(newRegion);
       },
-      (error) => {
+      error => {
         console.error('Error getting current position:', error);
         Alert.alert('Error', error.message);
       },
-      { enableHighAccuracy: true }
+      {enableHighAccuracy: true},
     );
   };
-  
 
-  const CheckmarkBox = ({ isChecked, onPress }) => (
+  const CheckmarkBox = ({isChecked, onPress}) => (
     <TouchableOpacity onPress={onPress} style={styles.checkboxContainer}>
       {isChecked && <Text style={styles.checkboxCheck}>✓</Text>}
     </TouchableOpacity>
@@ -165,61 +172,76 @@ const MapViewComponent = () => {
 
   return (
     <View style={styles.container}>
-      <MapView
+      {!showPosts ? (<MapView
         ref={mapRef}
         style={styles.map}
         initialRegion={{
           latitude: 39.739235,
-          longitude: -104.990250,
+          longitude: -104.99025,
           latitudeDelta: 40,
           longitudeDelta: 40,
         }}>
-        {markers.length > 0 && markers.map((marker, index) => (
-          <Marker
-            key={index}
-            identifier={marker.id} // Use identifier for fitToSuppliedMarkers
-            coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
-            title={marker.name}
-            description={marker.address}
-          >
-            <View style={styles.circle}>
-              {/* change "1" to reflect count for number of checkins */}
-              <Text style={styles.number}>{marker.id}</Text>
-            </View>
-            <Callout onPress={() => handlePress(marker)}>
-              <View style={styles.calloutView}>
-                <Text style={styles.calloutTitle}>{marker.name}</Text>
-                <Text style={styles.calloutDescription}>{marker.address}</Text>
-
-                <View style={styles.buttonContainer}>
-                  <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                    <CheckmarkBox
-                      isChecked={!!checkedIn[marker]}
-                      onPress={() => toggleCheckIn(marker)}
-                    />
-                    <Button title="Check-In" onPress={() => handleCheckIn(marker)} />
-                  </View>
-                  <TouchableOpacity style={{flexDirection: 'row', alignItems: 'center'}}
-                    onPress={()=> {
-                      if (isRecording) {
-                        stopRecordingAndPlayBack(marker.id);
-                      } else {
-                        startRecording();
-                      }
-                    }}>
-                    <Text>{isRecording ? "Stop" : "Memo"}</Text>
-                    <Icon name="mic" size={20} color="#00ace8" /> 
-                  </TouchableOpacity>
-                </View>
+        {markers.length > 0 &&
+          markers.map((marker, index) => (
+            <Marker
+              key={index}
+              identifier={marker.id} // Use identifier for fitToSuppliedMarkers
+              coordinate={{
+                latitude: marker.latitude,
+                longitude: marker.longitude,
+              }}
+              title={marker.name}
+              description={marker.address}>
+              <View style={styles.circle}>
+                {/* change "1" to reflect count for number of checkins */}
+                <Text style={styles.number}>{marker.id}</Text>
               </View>
-            </Callout>
-          </Marker>
-        ))}
+              <Callout onPress={() => handlePress(marker)}>
+                <View style={styles.calloutView}>
+                  <TouchableOpacity onPress={() => setShowPosts(true)}>
+                    <Text style={styles.calloutTitle}>{marker.name}</Text>
+                  </TouchableOpacity>
+                  <Text style={styles.calloutDescription}>
+                    {marker.address}
+                  </Text>
 
-      </MapView>
+                  <View style={styles.buttonContainer}>
+                    <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                      <CheckmarkBox
+                        isChecked={!!checkedIn[marker]}
+                        onPress={() => toggleCheckIn(marker)}
+                      />
+                      <Button
+                        title="Check-In"
+                        onPress={() => handleCheckIn(marker)}
+                      />
+                    </View>
+                    <TouchableOpacity
+                      style={{flexDirection: 'row', alignItems: 'center'}}
+                      onPress={() => {
+                        if (isRecording) {
+                          stopRecordingAndPlayBack(marker.id);
+                        } else {
+                          startRecording();
+                        }
+                      }}>
+                      <Text>{isRecording ? 'Stop' : 'Memo'}</Text>
+                      <Icon name="mic" size={20} color="#00ace8" />
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </Callout>
+            </Marker>
+          ))}
+      </MapView>) : (
+        <View >
+          {/* place PostsComponent */}
+          <Text size={'44px'}>Showing Posts</Text>
+        </View>
+      )}
       <View style={styles.locationButton}>
         <TouchableOpacity onPress={getCurrentLocation}>
-         {/* // <Text style={styles.buttonText}>My Location</Text>
+          {/* // <Text style={styles.buttonText}>My Location</Text>
           <ion-icon name="locate-outline"></ion-icon> */}
           <Icon name="my-location" size={30} color="#00ace8" />
         </TouchableOpacity>
@@ -280,7 +302,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     borderWidth: 1,
-    borderColor: "#00ace8",
+    borderColor: '#00ace8',
     marginRight: 8,
   },
   checkboxCheck: {
