58f3e1d96f5ef33411fce9f1e18b2ae43f2c9936
fegloff <fegloff@gmail.com>
2024-03-09 17:09:37 -0500

    fix config file

--- socialMap/SocialMap/src/config.ts
+++ socialMap/SocialMap/src/config.ts
@@ -1,6 +1,6 @@
 import Config from 'react-native-config';
 
 export default {
-  openai_url: 'http://127.0.0.1:5000/openai/upload-audio', // Config.OPENAI_URL ?? '',
+  openai_url: Config.OPENAI_URL ?? '', //
   googleMap_api_key: Config.GOOGLE_MAPS_API_KEY ?? 'N/A',
 };
